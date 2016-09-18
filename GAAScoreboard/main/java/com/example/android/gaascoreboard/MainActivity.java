0892b745b687556bcef89b2b2f09ef0200d0fe95
riedlblower <github@michaelsexton.com>
2016-09-18 16:51:23 +0100

    added total score

--- GAAScoreboard/main/java/com/example/android/gaascoreboard/MainActivity.java
+++ GAAScoreboard/main/java/com/example/android/gaascoreboard/MainActivity.java
@@ -13,11 +13,15 @@ public class MainActivity extends AppCompatActivity {
     static final String GOAL_SCORE_TEAM_B = "teamBGoalScore";
     static final String POINT_SCORE_TEAM_A = "teamAPointScore";
     static final String POINT_SCORE_TEAM_B = "teamBPointScore";
+    static final String TOTAL_SCORE_TEAM_A = "teamATotalScore";
+    static final String TOTAL_SCORE_TEAM_B = "teamBTotalScore";
 
     int goalScoreTeamA = 0;
     int goalScoreTeamB = 0;
     int pointScoreTeamA = 0;
     int pointScoreTeamB = 0;
+    int totalScoreTeamA = 0;
+    int totalScoreTeamB = 0;
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt(GOAL_SCORE_TEAM_B, goalScoreTeamB);
         savedInstanceState.putInt(POINT_SCORE_TEAM_A, pointScoreTeamA);
         savedInstanceState.putInt(POINT_SCORE_TEAM_B, pointScoreTeamB);
+        savedInstanceState.putInt(TOTAL_SCORE_TEAM_A, totalScoreTeamA);
+        savedInstanceState.putInt(TOTAL_SCORE_TEAM_B, totalScoreTeamB);
 
         // Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
@@ -43,11 +49,15 @@ public class MainActivity extends AppCompatActivity {
             goalScoreTeamB = savedInstanceState.getInt(GOAL_SCORE_TEAM_B);
             pointScoreTeamA = savedInstanceState.getInt(POINT_SCORE_TEAM_A);
             pointScoreTeamB = savedInstanceState.getInt(POINT_SCORE_TEAM_B);
+            totalScoreTeamA = savedInstanceState.getInt(TOTAL_SCORE_TEAM_A);
+            totalScoreTeamB = savedInstanceState.getInt(TOTAL_SCORE_TEAM_B);
             setContentView(R.layout.activity_main);
             displayGoalForTeamA(goalScoreTeamA);
             displayGoalForTeamB(goalScoreTeamB);
             displayPointForTeamA(pointScoreTeamA);
             displayPointForTeamB(pointScoreTeamB);
+            displayTotalForTeamA(totalScoreTeamA);
+            displayTotalForTeamB(totalScoreTeamB);
         }
     }
 
@@ -57,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
     public void addGoalForTeamA(View view) {
         goalScoreTeamA = goalScoreTeamA + 1;
         displayGoalForTeamA(goalScoreTeamA);
+        increaseTotalForTeamA(3);
     }
 
     /**
@@ -65,8 +76,16 @@ public class MainActivity extends AppCompatActivity {
     public void addPointForTeamA(View view) {
         pointScoreTeamA = pointScoreTeamA + 1;
         displayPointForTeamA(pointScoreTeamA);
+        increaseTotalForTeamA(1);
     }
 
+    /**
+     * This method is called when the total needs updating for Team A.
+     */
+    public void increaseTotalForTeamA(int score) {
+        totalScoreTeamA = totalScoreTeamA + score;
+        displayTotalForTeamA(totalScoreTeamA);
+    }
 
     /**
      * Displays the given goal score for Team A.
@@ -85,11 +104,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Displays the total score for Team A.
+     */
+    public void displayTotalForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_total_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
      * This method is called when the 'Goal' button is clicked for TeamB.
      */
     public void addGoalForTeamB(View view) {
         goalScoreTeamB = goalScoreTeamB + 1;
         displayGoalForTeamB(goalScoreTeamB);
+        increaseTotalForTeamB(3);
     }
 
     /**
@@ -98,8 +126,16 @@ public class MainActivity extends AppCompatActivity {
     public void addPointForTeamB(View view) {
         pointScoreTeamB = pointScoreTeamB + 1;
         displayPointForTeamB(pointScoreTeamB);
+        increaseTotalForTeamB(1);
     }
 
+    /**
+     * This method is called when the total needs updating for Team B.
+     */
+    public void increaseTotalForTeamB(int score) {
+        totalScoreTeamB = totalScoreTeamB + score;
+        displayTotalForTeamB(totalScoreTeamB);
+    }
 
     /**
      * Displays the given goal score for Team B.
@@ -118,6 +154,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Displays the total score for Team B.
+     */
+    public void displayTotalForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_total_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
      * This method is called when the 'Reset' button is clicked.
      */
     public void reset(View view) {
@@ -125,10 +169,14 @@ public class MainActivity extends AppCompatActivity {
         goalScoreTeamB = 0;
         pointScoreTeamA = 0;
         pointScoreTeamB = 0;
+        totalScoreTeamA = 0;
+        totalScoreTeamB = 0;
         displayGoalForTeamA(goalScoreTeamA);
         displayGoalForTeamB(goalScoreTeamB);
         displayPointForTeamA(pointScoreTeamA);
         displayPointForTeamB(pointScoreTeamB);
+        displayTotalForTeamA(totalScoreTeamA);
+        displayTotalForTeamB(totalScoreTeamB);
     }
 
 }
